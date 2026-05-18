58d6fb567f91cec3695af77fde9e0ce070300d01
Sulaiman <msulaiman2006student@gmail.com>
2026-05-18 09:10:06 +0530

    Booking and bookingService modules completed for Sprint 1

---
+++ src/main/java/com/hotel/reservation/service/BookingService.java
@@ -0,0 +1,22 @@
+package com.hotel.reservation.service;
+
+import com.hotel.reservation.model.Booking;
+import java.util.ArrayList;
+
+public class BookingService {
+
+    public void createBooking(Booking booking) {
+    }
+
+    public ArrayList<Booking> getBookings() {
+        return new ArrayList<>();
+    }
+
+    public void cancelBooking(String bookingId) {
+
+    }
+
+    public ArrayList<Booking> getBookingsByUser(String userId) {
+        return new ArrayList<>();
+    }
+}
